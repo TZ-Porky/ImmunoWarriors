6f46da3afbfe07a93e03043a3ef4902a5af7f3fa
TZ-Porky <btkextensions@gmail.com>
2025-06-03 21:16:54 +0100

    Ajout de l'initialisation de Hive et du plugin Path Provider, mise à jour des dépendances

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:immunowarriors/login_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
+  // Assure la connexion et l'initialisation aux différents services externes
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialiser Hive avec le bon dossier de stockage
+  final appDocumentDir = await getApplicationDocumentsDirectory();
+  await Hive.initFlutter(appDocumentDir.path);
+
+  // Ouvrir une box (boîte de stockage clé/valeur)
+  await Hive.openBox('myBox');
+
+  // Initialise la connexion à la Firebase
   await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
